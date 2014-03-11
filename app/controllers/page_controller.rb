8a93d3b97bae73ae45a6f537cc36ece2f9973e12
Mikeva <eva091960601@gmail.com>
2014-03-11 11:48:52 +0800

    first commit

---
+++ app/controllers/page_controller.rb
@@ -0,0 +1,45 @@
+class PageController < ApplicationController
+	def index
+		
+	end
+
+	def about
+
+	end
+
+	def recognition
+
+	end
+
+	def researchanddemos
+		
+	end
+
+	def papersandconferences
+		
+	end
+
+	def researchgrants
+		
+	end
+
+	def people
+		
+	end
+
+	def resourcesformisandbilab
+		
+	end
+
+	def photogallery
+		
+	end
+
+	def contactus
+		
+	end
+
+	def bilabintranet
+		
+	end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Web::Application.config.secret_token = '638d2bbfbc51a7099f8a753b49c70ca53490f5a2ae69d36eed8a1f9967fc1a71a8ed1a217933cc5737b7ce664ee23a089fd7c2e8615f3ad734c22f9a7db56c84'
